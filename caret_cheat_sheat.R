b0af294a179f7192a152c0613a00f829d11fc40e
Davis, Matthew <davismat@musc.edu>
2018-02-22 16:30:14 -0500

    week7 commit update to caret cheatsheet

---
+++ caret_cheat_sheat.R
@@ -0,0 +1,56 @@
+### Machine Learning with Caret
+# get data
+library(caret)
+library(fivethirtyeight)
+setwd("C:/Users/Davis/Documents/kaggle/titanic")
+
+set.seed(1)
+train.set <- read.csv("train.csv", stringsAsFactors=TRUE)
+test.set  <- read.csv("test.csv",  stringsAsFactors=TRUE)
+
+target = "Survived"
+xvars = c('Fare', 'SibSp', 'Parch', 'Age')
+# create one hot encoder dummy variables
+dmv = dummyVars(~Sex +Embarked +Pclass,train )
+pp = preProcess(train.set[,xvars], method = 'medianImpute')
+
+
+# create feature extraction function
+create_features = function(x, xvars, dmv, pp){
+  x1 = predict(dmv, x)  
+  x2 = predict(pp, x[, xvars])
+  output = data.frame(x1, x2)
+  colnames(output) = make.names(colnames(output))
+  return(output)
+}
+
+to_train = data.frame(create_features(train.set, xvars, dmv, pp),
+                      Class = as.factor(train[, target]))
+levels(to_train$Class) = c('d', 's')
+
+# create cntroller
+ctr <- trainControl(
+                  method = "cv",
+                  number = 5,
+                  search = 'random',
+                  classProbs = TRUE,
+                  summaryFunction = twoClassSummary,
+                  preProcOptions = list(pcaComp = 10))
+
+fit = train(Class~., 
+            data = to_train,
+            method = 'rpart',
+            preProcess = 'medianImpute',
+            trControl = ctr,
+            tuneLength = 5
+            )
+
+submission <- data.frame(PassengerId = test.set$PassengerId)
+test_features = create_features(test.set, xvars,  dmv, pp)
+submission$Survived <- predict(fit, test_features)
+write.csv(submission, file = "submission.csv", row.names=FALSE)
+
+
+
+
+
---
+++ week7.md
@@ -0,0 +1,22 @@
+#### Course Plan Week 7 Day 1
+  * Assignment Feed Back
+  * Review of Logistic regression
+  * Machine Learning with Caret
+ https://topepo.github.io/caret/index.html
+    + Preprocessing
+    + Splitting
+    + training
+    + tuning selectio
+
+#### Course Plan Week 6 Day 2
+  * R Markdowns
+  * Plotting with GGPLOT
+  
+#### Homework
+ create a R HTML Mark down upload to oaks.
+ The markdown should include:
+  + preprocessing using caret
+  + splitting using caret
+  + parameter tuning using caret
+  + visualition of the model performance
+  
